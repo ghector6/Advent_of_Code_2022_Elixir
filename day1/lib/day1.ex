44558e8332eca2b2f3f02979763da035f544c2b1
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-04 22:34:48 -0600

    refactor test and code to convert to int

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -9,5 +9,21 @@ defmodule Day1 do
     |> String.split("\n\n")
   end
 
+  def list_parser(list) do
+    #    [
+    #      ["1000", "2000", "3000"],
+    #      ["4000"],
+    #      ["5000", "6000"],
+    #      ["7000", "8000", "9000"],
+    #      ["10000"]
+    #    ]
+    list
+    |> Enum.map(&String.split(&1,"\n"))
+    |> Enum.map(fn inside -> Enum.map(inside, &String.to_integer/1) end)
+  end
+
+  def list_sum(list) do
+  end
+
 
 end
--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -20,4 +20,19 @@ defmodule Day1Test do
     10000
     """) == ["1000\n2000\n3000", "4000", "5000\n6000", "7000\n8000\n9000", "10000"]
   end
+
+  test "returns the list of lists to integer" do
+    assert Day1.list_parser(["1000\n2000\n3000", "4000", "5000\n6000", "7000\n8000\n9000", "10000"]) == [[1000, 2000, 3000], [4000], [5000, 6000],
+  [7000, 8000, 9000], [10000]]
+
+  end
+
+#  test "returns the sum of each list inside the list" do
+#    assert Day1.list_sum([["1000", "2000", "3000"], ["4000"], ["5000", "6000"],
+#  ["7000", "8000", "9000"], ["10000"]]) == []
+#  end
+
+
+
+
 end
