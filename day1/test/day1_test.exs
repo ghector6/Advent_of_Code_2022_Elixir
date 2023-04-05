b51e35a303377ccb2dbc5a4ca63b6da3dbf0bf1f
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-04 19:24:34 -0600

    add: test to convert to list

--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -2,7 +2,22 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Day1.hello() == :world
+  test "reads numbers inside the input and return them as a list" do
+    assert Day1.list_converter("""
+    1000
+    2000
+    3000
+
+    4000
+
+    5000
+    6000
+
+    7000
+    8000
+    9000
+
+    10000
+    """) == ["1000\n2000\n3000", "4000", "5000\n6000", "7000\n8000\n9000", "10000\n"]
   end
 end
