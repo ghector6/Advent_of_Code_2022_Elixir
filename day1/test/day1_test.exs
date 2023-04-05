b2e67c905b957bb925c853150ae79c759afb4977
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-04 22:39:18 -0600

    add: test for sum

--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -27,10 +27,10 @@ defmodule Day1Test do
 
   end
 
-#  test "returns the sum of each list inside the list" do
-#    assert Day1.list_sum([["1000", "2000", "3000"], ["4000"], ["5000", "6000"],
-#  ["7000", "8000", "9000"], ["10000"]]) == []
-#  end
+  test "returns the sum of each list inside the list" do
+    assert Day1.list_sum([[1000, 2000, 3000], [4000], [5000, 6000],
+  [7000, 8000, 9000], [10000]]) == [6000, 4000, 11000, 24000, 10000]
+  end
 
 
 
