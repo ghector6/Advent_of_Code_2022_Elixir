214d11745144762b6970fcc195176971e1be39d1
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-28 17:29:01 -0600

    add: code returning correct answer

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,7 +1,21 @@
 defmodule Day1 do
+
   @moduledoc """
   Documentation for `Day1`.
   """
+  def  handler() do
+   reader()
+   |> list_converter()
+   |> list_parser()
+   |> list_sum()
+   |> total()
+  end
+
+  @boo Path.expand("../input_data.txt", __DIR__)
+  def reader() do
+    @boo
+    |> File.read!()
+  end
 
   def list_converter(input) do
     input
@@ -23,6 +37,11 @@ defmodule Day1 do
   end
 
   def list_sum(list) do
+    Enum.map(list, &Enum.sum/1)
+  end
+
+  def total(list) do
+   Enum.max(list)
   end
 
 
