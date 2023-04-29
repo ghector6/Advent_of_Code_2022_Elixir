6b3302b8677c98672d1a6a07659d71f065f49cda
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-29 00:18:35 -0600

    add: code for game fn

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -2,11 +2,20 @@ defmodule Day2 do
   @moduledoc """
   Documentation for `Day2`.
   """
+  @posibilities  %{
+    "A X" => 4, "A Y" => 8, "A Z" => 3,
+    "B X" => 1, "B Y" => 5, "B Z" => 9,
+    "C X" => 7, "C Y" => 2, "C Z" => 6
+  }
+   def parse(string)  do
+     string
+     |> String.trim()
+     |> String.split("\n")
+     |> Enum.map(&String.trim/1)
+   end
 
- def parse(string)  do
-   string
-   |> String.trim()
-   |> String.split("\n")
-   |> Enum.map(&String.trim/1)
- end
+  def game(list) do
+    for round <- list, do:  @posibilities[round]
+  end
+
 end
