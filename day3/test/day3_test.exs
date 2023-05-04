2afe20ed36226b2c90b2c3d0f6efb197457fcd45
Hector Garcia Rosales <ghectormus@gmail.com>
2023-05-03 19:53:18 -0600

    test: writing the count fn test

--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -2,7 +2,7 @@ defmodule Day3Test do
   use ExUnit.Case
   doctest Day3
 
-  test "greets the world" do
-    assert Day3.hello() == :world
+  test "returns 12 as the length of this vJrwpWtwJgWrhcsFMMfFFhFp" do
+    assert Day3.counter("vJrwpWtwJgWrhcsFMMfFFhFp") == 12
   end
 end
